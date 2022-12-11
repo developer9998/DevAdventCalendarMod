749e4fbe0a33d691dac63fb798e2ead32bfd4d77
dev9998 <81720436+developer9998@users.noreply.github.com>
2022-12-11 12:57:57 -0330

    bump version

--- DevAdventCalendarMod/DevAdventCalendarMod/PluginInfo.cs
+++ DevAdventCalendarMod/DevAdventCalendarMod/PluginInfo.cs
@@ -1,12 +1,9 @@
 ﻿namespace DevAdventCalendarMod
 {
-    /// <summary>
-    /// This class is used to provide information about your mod to BepInEx.
-    /// </summary>
     internal class PluginInfo
     {
         public const string GUID = "com.dev9998.gorillatag.devadventcalendarmod";
         public const string Name = "DevAdventCalendarMod";
-        public const string Version = "1.0.0";
+        public const string Version = "1.1.0";
     }
 }
